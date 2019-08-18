ec401807758fb6d54f05a3dff697f2cc3ca6bcea
Dominik Horn <horn.dominik101@yahoo.de>
2019-08-18 15:06:18 +0200

    Initial Commit

---
+++ TerraGen/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TerraGen
+//
+//  Created by Dominik Horn on 8/18/19.
+//  Copyright © 2019 Dominik Horn. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
